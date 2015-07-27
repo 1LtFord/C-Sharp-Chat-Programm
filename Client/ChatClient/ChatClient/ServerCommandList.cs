69d1ffe9c0d3ab523c9c6f8314b0523358d01583
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-27 23:15:11 +0300

    add ServerCommandList enum

---
+++ Client/ChatClient/ChatClient/ServerCommandList.cs
@@ -0,0 +1,25 @@
+﻿namespace ChatClient
+{
+    public enum ServerCommandList
+    {
+        IsLogged,
+        WrongPwd,
+        NewUserRegistered,
+        ServerIsFull,
+        ServerIsPrivate,
+        UserIsBanned,
+        LoggedUserList,
+        SendLatestMsgLog,
+        MsgLogRestricted,
+        MsgAccepted,
+        DisconnectReceived,
+        SendServerInfo,
+        SpreadMsg,
+        ServerIsClosing,
+        WrongArgs,
+        AlreadyLogged,
+        NotLoggedIn,
+        ChangeOfLoggedUserList,
+        ChangeOfServerInfo
+    }
+}
