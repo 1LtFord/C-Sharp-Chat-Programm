13ba081aa2f88041ec0ccb27fe9bef6063f9c708
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-27 22:54:23 +0300

    create ClientCommandList enum

---
+++ Client/ChatClient/ChatClient/ClientCommandList.cs
@@ -0,0 +1,24 @@
+﻿namespace ChatClient
+{
+    public enum ClientCommandList
+    {
+        Connect,
+        SendMsg, // (1,Nachricht)
+        Disconnect, // (2)
+        ChangeName,
+        Login, // (4;Benutzername;BenutzerPW[;ServerPW])
+        GetLatestMsgLog, // (5)
+        GetLoggedUserList, // (6)
+        GetAllUserList,
+        SetMyStatus,
+        GetMyStatus,
+        KickUser,
+        BanUser,
+        SetUserRole,
+        UnbanUser,
+        SendPrivateMsg,
+        ChangePwd,
+        GetServerInfo, // (16)
+        GetServerReadme
+    }
+}
