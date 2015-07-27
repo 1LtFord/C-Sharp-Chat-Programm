5c10151dc7b1ce0261f7ed9eb4b22d4ba2a3df28
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-28 00:09:10 +0300

    refactoring all and use StringBuilder in ToString method (faster is)

--- Client/ChatClient/ChatClient/Message.cs
+++ Client/ChatClient/ChatClient/Message.cs
@@ -1,49 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChatClient
+﻿namespace ChatClient
 {
+    using System;
+    using System.Text;
+
     public class Message
     {
-        public string Author;
-        public string Text;
-        public string Date;
+        private string author;
+        private string text;
+        private string date;
 
-        public Message() { }
-
-        public Message(string _author, string _text, string _date)
+        public Message()
         {
-            fetchMessage(_author, _text, _date);
         }
 
-        public Message(string MsgString)
+        public Message(string author, string text, string date)
+        {
+            this.FetchMessage(author, text, date);
+        }
+
+        public Message(string message)
         { 
-            string[] buff=MsgString.Split(';');
-            fetchMessage(decText(buff[0]), decText(buff[1]), decText(buff[2]));
+            string[] buff = message.Split(';');
+            this.FetchMessage(this.UnscapeText(buff[0]), this.UnscapeText(buff[1]), this.UnscapeText(buff[2]));
         }
 
 
-        public void fetchMessage(string _author, string _text, string _date)
+        public void FetchMessage(string author, string text, string date)
         {
-            Author = _author;
-            Text = _text;
-            Date = _date;
+            this.author = author;
+            this.text = text;
+            this.date = date;
         }
 
         public override string ToString()
         {
-            return encText(Author) + ";" + encText(Text) + ";" + encText(Date);
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(this.EscapeText(this.author));
+            stringBuilder.Append(";");
+            stringBuilder.Append(this.EscapeText(this.text));
+            stringBuilder.Append(";");
+            stringBuilder.Append(this.EscapeText(this.date));
+            stringBuilder.Append(";");
+
+            return stringBuilder.ToString();
         }
 
-        public string decText(string text)
+        public string UnscapeText(string text)
         {
             return Uri.UnescapeDataString(text);
         }
 
-        public string encText(string text)
+        public string EscapeText(string text)
         {
             return Uri.EscapeDataString(text);
         }
