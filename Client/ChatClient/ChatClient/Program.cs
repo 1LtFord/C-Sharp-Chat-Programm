590c370ac4b03ef412f8369f9571e50a33bb44a0
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-28 00:09:32 +0300

    remove unessesary using and comments

--- Client/ChatClient/ChatClient/Program.cs
+++ Client/ChatClient/ChatClient/Program.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace ChatClient
+﻿namespace ChatClient
 {
-    static class Program
+    using System;
+    using System.Windows.Forms;
+
+    public static class Program
     {
-        
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
